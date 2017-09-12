2db2750bb3438414b29938e96108244d88dc7cc2
jo3t3ch3m <joe@techemstudios.com>
2017-09-12 16:52:52 -0400

    adding the topics page

--- project/projects/urls.py
+++ project/projects/urls.py
@@ -7,4 +7,7 @@ from . import views
 urlpatterns = [
     # Home page.
     url(r'^$', views.index, name='index'),
+    
+    # Show all Topics
+    url(r'^topics/$', views.topics, name ='topics'),
 ]
--- project/projects/views.py
+++ project/projects/views.py
@@ -3,8 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import Topic
+
 # Create your views here.
 
 def index(request):
 	"""The home page for Project"""
 	return render(request, 'projects/index.html')
+	
+def topics(request):
+	"""Show all topics."""
+	topics = Topic.objects.order_by('date_added')
+	context = {'topics': topics}
+	return render(request, 'projects/topics.html', context)
